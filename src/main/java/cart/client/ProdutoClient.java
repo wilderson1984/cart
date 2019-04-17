93903bde8e026050e87323caaef7a110161bd123
root <root@cart.c.cloud-opet.internal>
2019-04-17 17:00:17 +0000

    change ip

--- src/main/java/cart/client/ProdutoClient.java
+++ src/main/java/cart/client/ProdutoClient.java
@@ -12,7 +12,7 @@ public class ProdutoClient {
     private RestOperations restOperations;
     private String productServiceUrl;
 
-    public ProdutoClient(RestOperations restOperations, @Value("http://35.247.229.188:8888") String productServiceUrl) {
+    public ProdutoClient(RestOperations restOperations, @Value("http://35.247.233.247:8888") String productServiceUrl) {
         this.restOperations = restOperations;
         this.productServiceUrl = productServiceUrl;
     }
